1786fed0b0efde30f87152a34a49288fbbc790a6
Pavan-FullstackDeveloper <pavanthoti786@gmail.com>
2024-01-13 18:18:59 +0530

    it is created using useState built in function

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,9 @@
 import React from 'react'
-import Card from "./components/Card"
-
-import "./App.css"
-
-const cardsList = [
-    {
-      id: 1,
-      title: 'Data Scientist',
-      description:
-        'Data scientists gather and analyze large sets of structured and unstructured data',
-      imgUrl: 'https://assets.ccbp.in/frontend/react-js/data-scientist-img.png',
-      className: 'card-1',
-    },
-    {
-      id: 2,
-      title: 'IOT Developer',
-      description:
-        'IoT Developers are professionals who can develop, manage, and monitor IoT devices.',
-      imgUrl: 'https://assets.ccbp.in/frontend/react-js/iot-developer-img.png',
-      className: 'card-2',
-    },
-    {
-      id: 3,
-      title: 'VR Developer',
-      description:
-        'A VR developer creates completely new digital environments that people can see.',
-      imgUrl: 'https://assets.ccbp.in/frontend/react-js/vr-developer-img.png',
-      className: 'card-3',
-    },
-    {
-      id: 4,
-      title: 'ML Engineer',
-      description:
-        'Machine learning engineers feed data into models defined by data scientists.',
-      imgUrl: 'https://assets.ccbp.in/frontend/react-js/ml-engineer-img.png',
-      className: 'card-4',
-    }
-  ]
-
+import Counter from "./components/Counter"
 
 const App = () => {
   return (
-    <div className="home-bg">
-        <h1 className="heading">Learn 4.0 Technologies</h1>
-        <p>Get trained by alumni of IITS and top companies like amazon.microSoft,intel, Nvidia,Qualcommn,etc</p>
-        <ul>
-           <li className="list ul-style">{cardsList.map(eachCard=><Card cardDetails={eachCard} key={eachCard.id}/>)}</li>
-        </ul>
-        
-    </div>
+    <Counter/>
   )
 }
 
